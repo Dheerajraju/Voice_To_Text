fb654dbec0fd12832f556c0ec58c52fec19365ea
Dheeraj raju <dheshal07@gmail.com>
2026-07-09 18:34:10 +0530

    Update ui_events.h

--- Main/ui_events.h
+++ Main/ui_events.h
@@ -1,5 +1,28 @@
-#pragma once
+/*
+ * ============================================================
+ * ESP32-P4 AI Voice Assistant
+ * Stage 4.1
+ * UI Events Header
+ * ============================================================
+ */
+
+#ifndef UI_EVENTS_H
+#define UI_EVENTS_H
 
 #include "lvgl.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*-------------------------------------------------------------
+ * Event Callbacks
+ *------------------------------------------------------------*/
+
 void mic_btn_event_cb(lv_event_t *e);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UI_EVENTS_H */

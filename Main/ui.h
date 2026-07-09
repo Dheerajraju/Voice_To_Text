17bbe80d888b6fde91fff11addab0312b9d7edbb
Dheeraj raju <dheshal07@gmail.com>
2026-07-09 11:14:28 +0530

    Update ui.h

--- Main/ui.h
+++ Main/ui.h
@@ -1,1 +1,15 @@
-/*****ui.h*****/
+#pragma once
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ui_create(void);
+void ui_set_status(const char *text);
+void ui_set_indicator_color(lv_color_t color);
+
+#ifdef __cplusplus
+}
+#endif

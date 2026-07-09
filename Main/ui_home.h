148588e5c9e87d9ad89cff7b7a461d3080e66f83
Dheeraj raju <dheshal07@gmail.com>
2026-07-09 18:35:12 +0530

    Create ui_home.h

---
+++ Main/ui_home.h
@@ -0,0 +1,26 @@
+/*
+ * ============================================================
+ * ESP32-P4 AI Voice Assistant
+ * Stage 4.1
+ * Home Screen Header
+ * ============================================================
+ */
+
+#ifndef UI_HOME_H
+#define UI_HOME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*-------------------------------------------------------------
+ * Create Home Screen
+ *------------------------------------------------------------*/
+
+void ui_home_create(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UI_HOME_H */
